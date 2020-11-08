de2b9f9743bc5448dedd2056135aa06129a08b45
RosarioTG <ro.tani.gomez@gmail.com>
2020-11-08 18:17:57 -0300

    test edit Animal

---
+++ tests/Browser/EditAnimalTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Browser;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class EditTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testEditAnimal()
+    {
+        $user =User::factory() ->create([
+            'email'=> 'test@laravel.com',
+            'password' => bcrypt('12345678'),
+            'role' => 'manager'
+        ]);
+        $animal =Animal::factory() ->create([
+            'user_id ' => $user -> id
+        ]);
+        $specie =Specie::factory() ->create();
+        $this->browse(function (Browser $browser) use ($user , $specie,$animal) {
+        $browser->visit('/login')
+              ->type('email',$user->email)
+              ->type('password',$user->password)
+              ->press('LOGIN')
+              ->visit('animal')
+                 ->assertSee($animal->name )
+                 ->click('@edit')
+                 ->type('name', 'Edit Dusk')
+                 ->type('description', 'This is edit with dusk')
+                 ->press('Insertar')
+                ->assertSee('Edit Dusk');
+        });
+    }
+}
