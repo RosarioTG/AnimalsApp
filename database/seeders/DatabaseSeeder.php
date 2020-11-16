7b6f2af2b8b91ea5be56d71cee7951ba0c199b7a
Agus Escalante <62969018+agusescalante@users.noreply.github.com>
2020-11-16 17:16:26 -0300

    issue of api-json data (#75) (Closes #73)
    * issue of api-json data
    
    * issue api
    
    * issue api2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      $this -> call(SpecieSeeder::class);
-      $this -> call(AnimalSeeder::class);
+      // $this -> call(SpecieSeeder::class);
+      // $this -> call(AnimalSeeder::class);
       $this -> call(UserSeeder::class);
     }
 }
---
+++ tests/Feature/AnimalApiTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Animal;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class AnimalApiTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testUserCanReadAnimalsCreated()
+    {
+        $user = User::factory()->create();
+        $animal = Animal::factory()->create(['user_id' => $user->id]);
+        $response=$this->actingAs($user)->getJson('api/animal');
+        
+        $response->assertStatus(200);
+        $response->assertJsonCount(1);
+        $response->assertJsonFragment([
+            "name" => $animal->name]);
+        $response->assertJsonFragment(
+        ['user_id'=>$user->id]
+        );
+
+    }
+
+
+    public function testUserCantReadAnimalsCreated()
+    {
+        $userOne = User::factory()->create();
+        $userTwo = User::factory()->create();
+
+        $animal = Animal::factory()->create(['user_id' => $userTwo->id]);
+      
+        $response = $this->actingAs($userOne)->getJson('/api/animal');
+        $response->assertJsonMissing([
+            "name" => $animal->name]);
+        $response->assertStatus(200);
+        $response->assertJsonCount(1);
+
+    }
+
+}
