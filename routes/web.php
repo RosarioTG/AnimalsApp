1815c3f645c14668512bb74bb9523fcf292562a2
RosarioTG <ro.tani.gomez@gmail.com>
2020-11-16 22:48:31 -0300

    delete home controller

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Input;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalsController;
 use App\Http\Controllers\homeController;
 use App\Http\Controllers\SpecieController;
+use App\Models\Animal;
+use App\Models\Specie;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,4 +33,8 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::resource('specie',SpecieController::class);
 });
-Route::resource('/',homeController::class);
+Route::get('/',function () {
+          $species = Specie::with('animals') -> get();
+         $animals = Animal::all();
+        return view('welcome',['species'=> $species ,'animals'=> $animals ]);
+})->name('welcome');
